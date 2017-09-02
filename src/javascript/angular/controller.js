acd9162d217720703f2c368ba984c601af65a6f3
Kaori Yatsu <s1210103@gmail.com>
2017-09-02 12:56:40 +0900

    [fix]Include CSS and JS files to webpack

--- src/javascript/angular/controller.js
+++ src/javascript/angular/controller.js
@@ -1,10 +1,12 @@
 let angModule = require('./module');
 require('./config');
+require('./factory');
+require('./filter');
 
-angModule.angApp.controller("webpackEnvironmentCtrl", ['$scope', "$log", function($scope, $log) {
+angModule.angApp.controller("webpackEnvironmentCtrl", ['$scope', "$log", 'DemoFactory', 'HttpResponseFactory', function($scope, $log, DemoFactory, HttpResponseFactory) {
     $scope.init = function() {
         $log.debug('Webpack Environment!!!!!!!!!!!');
-        $scope.demo = 'Webpack Environment!!!!!!!!!!!';
+        $scope.demo = DemoFactory.demo();
     };
 
 }]);
