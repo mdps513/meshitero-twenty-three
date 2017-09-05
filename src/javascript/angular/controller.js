7741c64b9ea97991ac3af8871926edc8eca6c556
Kaori Yatsu <s1210103@gmail.com>
2017-09-05 11:01:12 +0900

    [update]Change the clock timer from default

--- src/javascript/angular/controller.js
+++ src/javascript/angular/controller.js
@@ -1,12 +1,8 @@
 let angModule = require('./module');
-require('./config');
-require('./factory');
-require('./filter');
 
-angModule.angApp.controller("webpackEnvironmentCtrl", ['$scope', "$log", 'DemoFactory', 'HttpResponseFactory', function($scope, $log, DemoFactory, HttpResponseFactory) {
-    $scope.init = function() {
-        $log.debug('Webpack Environment!!!!!!!!!!!');
-        $scope.demo = DemoFactory.demo();
-    };
-
+angModule.angApp.controller("webpackEnvironmentCtrl", ['$scope', '$interval', function( $scope, $interval ) {
+    $scope.count = 0;
+    var timer = $interval(function() {
+        $scope.count = (new Date()).toLocaleTimeString();
+    }, 1000);
 }]);
